03963512e6a5856fcc67ef2627592ba90d53b47d
Yash Jain <yashjain07.yj@gmail.com>
2026-08-10 20:30:01 +0530

    Completed The Github Workflow For The Deployment.

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex items-center justify-center min-h-screen text-4xl font-bold'>
+
+      Deploying Project With Github Actions
+
+    </div>
+    
+  )
+}
+
+export default page
