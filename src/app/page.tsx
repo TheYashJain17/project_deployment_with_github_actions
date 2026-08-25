31e85826e1eda7671bcd1eaf847b6eb2bbd88f28
Yash Jain <yashjain07.yj@gmail.com>
2026-08-25 18:30:05 +0530

    Made Some Changes In Page.tsx.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className='flex items-center justify-center min-h-screen text-4xl font-bold'>
 
-      Its working
+    So Finally Goodbye World
 
     </div>
     
