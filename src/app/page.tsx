ba2d4c0dce479777eb779f98c0ec3a8e9dab15f6
Yash Jain <yashjain07.yj@gmail.com>
2026-08-26 20:21:51 +0530

    Established Relation Between Build Job And Sonarcube Job, If Sonarcube Job Is Success, Then Only Will Move Forward.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className='flex items-center justify-center min-h-screen text-4xl font-bold'>
 
-    So Finally Goodbye World with sonarcube scan
+    So Finally Goodbye World
 
     </div>
     
